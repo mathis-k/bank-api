448f72badb9f14b1fd2970de338ff4e7b42da3f4
mathis <mathis.kengni@tum.de>
2024-08-07 23:44:56 +0200

    added handleFunctions plus more functionality

--- api.go
+++ api.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type APIServer struct {
+	listenAddr string
+}
+type APIError struct {
+	Message string `json:"message"`
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handlecCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mathis-k/bank-api
 
 go 1.22.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
