7248310bcae23e87ba6c6e162329e656945666eb
mathis <mathis.kengni@tum.de>
2024-08-08 07:53:03 +0200

    created types.go made API-Endpoints functional

--- api.go
+++ api.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type apiFunc func(w http.ResponseWriter, r *http.Request) error
+type APIServer struct {
+	listenAddress string
+}
+type APIResponse struct {
+	Message string `json:"message"`
+}
 
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -17,23 +25,24 @@ func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	}
 }
 
-type APIServer struct {
-	listenAddress string
-}
-
-func NewAPI(listenAddress string) *APIServer {
+func NewAPIServer(listenAddress string) *APIServer {
 	return &APIServer{
 		listenAddress: listenAddress,
 	}
 }
 func (s *APIServer) Run() {
+
 	router := mux.NewRouter()
-	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount)).
+		Methods(http.MethodGet, http.MethodPost)
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID)).
+		Methods(http.MethodGet, http.MethodDelete, http.MethodPut)
+
+	log.Printf("API server is running on localhost%s ... 🚀", s.listenAddress)
 	err := http.ListenAndServe(s.listenAddress, router)
 	if err != nil {
 		panic(fmt.Errorf("%w: Error starting server: %v", err, s.listenAddress))
 	}
-	log.Println("API server is running on", s.listenAddress)
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
@@ -42,20 +51,60 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 		return s.handleCreateAccount(w, r)
 	case http.MethodGet:
 		return s.handleGetAccounts(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return fmt.Errorf("method not allowed")
+	}
+}
+func (s *APIServer) handleAccountByID(w http.ResponseWriter, r *http.Request) error {
+	switch r.Method {
+	case http.MethodGet:
+		return s.handleGetAccountByID(w, r)
 	case http.MethodDelete:
 		return s.handleDeleteAccount(w, r)
+	case http.MethodPut:
+		return nil
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return fmt.Errorf("method not allowed")
 	}
 }
 func (s *APIServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) error {
+	accounts := []Account{
+		*NewAccount("John", "Doe", "john.doe@example.com"),
+		*NewAccount("Jane", "Doe", "jane.doe@example.com"),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(accounts); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return fmt.Errorf("error encoding response: %w", err)
+	}
 	return nil
 }
 func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error {
+	//id := mux.Vars(r)["id"]
+	account := NewAccount("John", "Doe", "john.doe@example.com")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(account); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return fmt.Errorf("error encoding response: %w", err)
+	}
 	return nil
 }
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	msg := fmt.Sprintf("Account with ID: %d deleted successfully!", id)
+	if err := json.NewEncoder(w).Encode(APIResponse{msg}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return fmt.Errorf("error encoding response: %w", err)
+	}
 	return nil
 }
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
---
+++ types.go
@@ -0,0 +1,29 @@
+package main
+
+import "math/rand"
+
+type Account struct {
+	ID        int     `json:"id"`
+	FirstName string  `json:"first_name"`
+	LastName  string  `json:"last_name"`
+	Email     string  `json:"email"`
+	Number    int64   `json:"number"`
+	Balance   float64 `json:"balance"`
+}
+
+func NewAccount(firstName, lastName, email string, balance ...float64) *Account {
+	var initialBalance float64
+	if len(balance) > 0 {
+		initialBalance = balance[0]
+	} else {
+		initialBalance = 0
+	}
+	return &Account{
+		ID:        rand.Intn(1000),
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Number:    int64(rand.Intn(1000000)),
+		Balance:   initialBalance,
+	}
+}
