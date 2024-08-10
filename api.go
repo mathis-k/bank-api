2b3e9b3ea52a88bc5d6150a2cb0c41cd8931a81d
Mathis Bryan Kengni <skymkengni1@gmail.com>
2024-08-10 20:02:02 +0200

    bug fixing

--- api.go
+++ api.go
@@ -19,7 +19,7 @@ type APIResponse struct {
 	Message string `json:"message"`
 }
 
-func json_message(w http.ResponseWriter, code int, message string) {
+func jsonMessage(w http.ResponseWriter, code int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	if err := json.NewEncoder(w).Encode(APIResponse{message}); err != nil {
@@ -48,14 +48,16 @@ func (s *APIServer) Run() {
 	router.HandleFunc("/account/{id}", s.handleAccountByID).
 		Methods(http.MethodGet, http.MethodDelete, http.MethodPut)
 
-	log.Printf("API server is running on localhost%s ... 🚀", s.listenAddress)
+	log.Printf("✔ API server is running on localhost%s/ ... 🚀", s.listenAddress)
 	err := http.ListenAndServe(s.listenAddress, router)
 	if err != nil {
-		log.Panicf("✖ %w: Error starting server: %v", err, s.listenAddress)
+		log.Printf("✖ %w: Error starting server: %v", err, s.listenAddress)
+		log.Println("... Shutting down server ...")
+		return
 	}
 }
 func (s *APIServer) handleStartPage(w http.ResponseWriter, r *http.Request) {
-	// Set the content type to plain text
+
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	if _, err := w.Write([]byte(welcomeMessage)); err != nil {
@@ -69,7 +71,7 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		s.handleGetAccounts(w, r)
 	default:
-		json_message(w, http.StatusMethodNotAllowed, "Method not allowed")
+		jsonMessage(w, http.StatusMethodNotAllowed, "Method not allowed")
 		return
 	}
 }
@@ -80,7 +82,7 @@ func (s *APIServer) handleAccountByID(w http.ResponseWriter, r *http.Request) {
 	case http.MethodDelete:
 		s.handleDeleteAccount(w, r)
 	case http.MethodPut:
-		json_message(w, http.StatusNotImplemented, "Method not implemented yet")
+		jsonMessage(w, http.StatusNotImplemented, "Method not implemented yet")
 		return
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -90,12 +92,12 @@ func (s *APIServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) {
 
 	accounts, err := s.database.GetAllAccounts()
 	if err != nil {
-		json_message(w, http.StatusInternalServerError, err.Error())
+		jsonMessage(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	if len(accounts) == 0 {
-		json_message(w, http.StatusNoContent, "No accounts found")
+		jsonMessage(w, http.StatusNoContent, "No accounts found")
 		return
 	}
 
@@ -103,59 +105,59 @@ func (s *APIServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(accounts); err != nil {
-		json_message(w, http.StatusInternalServerError, err.Error())
+		jsonMessage(w, http.StatusInternalServerError, err.Error())
 	}
 }
 func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
-		json_message(w, http.StatusBadRequest, err.Error())
+		jsonMessage(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(id); err != nil {
-		json_message(w, http.StatusInternalServerError, err.Error())
+		jsonMessage(w, http.StatusInternalServerError, err.Error())
 	}
 }
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
-		json_message(w, http.StatusBadRequest, err.Error())
+		jsonMessage(w, http.StatusBadRequest, err.Error())
 		return
 	}
-	json_message(w, http.StatusOK, fmt.Sprintf("Account with ID: %d deleted successfully!", id))
+	jsonMessage(w, http.StatusOK, fmt.Sprintf("Account with ID: %d deleted successfully!", id))
 }
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) {
 	var req models.CreateAccountRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		json_message(w, http.StatusBadRequest, err.Error())
+		jsonMessage(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	if req.FirstName == "" {
-		json_message(w, http.StatusBadRequest, "Missing Firstname")
+		jsonMessage(w, http.StatusBadRequest, "Missing Firstname")
 		return
 	} else if req.LastName == "" {
-		json_message(w, http.StatusBadRequest, "Missing Lastname")
+		jsonMessage(w, http.StatusBadRequest, "Missing Lastname")
 		return
 	} else if req.Email == "" {
-		json_message(w, http.StatusBadRequest, "Missing Email")
+		jsonMessage(w, http.StatusBadRequest, "Missing Email")
 		return
 	}
 
 	account, err := models.NewAccount(&req)
 	if err != nil {
-		json_message(w, http.StatusBadRequest, err.Error())
+		jsonMessage(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	if err := s.database.CreateAccount(account); err != nil {
 		if err.Error() == fmt.Sprintf("an account with the email %s already exists", req.Email) {
-			json_message(w, http.StatusConflict, err.Error())
+			jsonMessage(w, http.StatusConflict, err.Error())
 		} else {
-			json_message(w, http.StatusInternalServerError, err.Error())
+			jsonMessage(w, http.StatusInternalServerError, err.Error())
 		}
 		return
 	}
@@ -163,7 +165,7 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	if err := json.NewEncoder(w).Encode(account); err != nil {
-		json_message(w, http.StatusInternalServerError, err.Error())
+		jsonMessage(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
@@ -173,7 +175,7 @@ func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) {
 }
 
 const welcomeMessage = `Welcome to the Bank JSON API Server! :)
-🏦
+
 Available endpoints:
 GET /account - get all accounts
 POST /account - create a new account
